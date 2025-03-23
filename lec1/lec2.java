fa732b1161bfb5583a0d7271921326d8f08ff23c
Sheikh Wasimuddin <161950279+sheikhwasimuddin@users.noreply.github.com>
2025-03-23 11:52:36 +0530

    Add files via upload

---
+++ lec1/lec2.java
@@ -0,0 +1,18 @@
+public class lec2 {
+    public static void main(String[] args) {
+        int a = 10;
+        boolean b=true;
+        char c='a';
+        byte d=4;
+        short e=10;
+        long f=10000000000L;
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        System.out.println("d = "+d);
+        System.out.println("e = "+e);
+        System.out.println("f = "+f);
+        
+
+    }
+}
